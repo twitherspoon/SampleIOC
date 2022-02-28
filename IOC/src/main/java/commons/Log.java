0cf4b9294053b2e814abe559743993e73316279c
abrar1001 <98736390+abrar1001@users.noreply.github.com>
2022-02-28 18:13:39 +0530

    Update Log.java

--- IOC/src/main/java/commons/Log.java
+++ IOC/src/main/java/commons/Log.java
@@ -27,7 +27,7 @@ public class Log {
 
 	public static void setup(String fileName) {
 
-		spark = new ExtentSparkReporter("Reports/"+fileName+"_"+SeleniumHelper.timeStamp()+".html");
+		htmlReporter = new ExtentHtmlReporter("Reports/"+"index"+".html");
 		report = new ExtentReports();		
 		test = report.createTest(fileName);
 
